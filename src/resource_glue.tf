a42a1066740e5b105fe07367d45ddbe86258bd00
evans-nyang <nyanggithub@gmail.com>
2023-12-12 15:27:34 +0300

    #14 Add glue tranformation job, update variables.tf, add terraform.tfvars.example file

--- src/resource_glue.tf
+++ src/resource_glue.tf
@@ -15,3 +15,24 @@ resource "aws_glue_crawler" "sdl_glue_crawler" {
     path = "${data.aws_s3_bucket.sdl_bucket.arn}/raw/*"
   }
 }
+
+resource "aws_glue_job" "transform_json_to_parquet" {
+  name     = "transform-json-to-parquet"
+  role_arn = data.aws_iam_role.sdl_glue_role.arn
+  command {
+    name            = "glueetl"
+    script_location = "s3://sdl-immersion-day-${var.aws_account_id}/scripts/transform-json-to-parquet.py"
+  }
+
+  glue_version      = "3.0"
+  worker_type       = "G.1X"
+  number_of_workers = 10 # You can adjust this based on your needs
+
+  execution_property {
+    max_concurrent_runs = 1
+  }
+
+  default_arguments = {
+    "--job-language" = "python"
+  }
+}
--- src/variables.tf
+++ src/variables.tf
@@ -5,41 +5,34 @@ locals {
 variable "region" {
   description = "AWS region for deployment of resources"
   type        = string
-  default     = "eu-west-1" # Update with your desired AWS region
 }
 
 variable "bucket_name" {
   description = "The name of S3 bucket"
   type        = string
-  default     = "sdl-immersion-day-123456789012" # Update with your desired bucket name
 }
 
 variable "environment" {
   description = "Environment setup either dev or prod"
   type        = string
-  default     = "dev" # Update with your desired environment name
 }
 
 variable "aws_account_id" {
   description = "AWS account number"
   type        = string
-  default     = "123456789012" # Update with your AWS account ID
 }
 
 variable "firehose_stream_name" {
   description = "Name of kinesis firehose data stream"
   type        = string
-  default     = "sdl-firehose-stream"
 }
 
 variable "database_name" {
   description = "Name of glue crawler database"
   type        = string
-  default     = "sdl-database"
 }
 
 variable "crawler_name" {
   description = "Name of glue crawler"
   type        = string
-  default     = "sdl-crawler"
 }
